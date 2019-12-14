7906bdadd33f701084944479d5a99fa53798f70c
jvdwijk <joeyvdwijk@gmail.com>
2019-12-14 13:26:36 +0100

    Throw it in the fire

---
+++ Assets/Scripts/Player/Detectors/InventoryDeposit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryDeposit : Detector
+{
+    [SerializeField]
+    private LocalInventory localInventory;
+    protected override void OnTriggerStay(Collider collider)
+    {
+        if (!CheckCollider(collider.gameObject) || !Input.GetKeyDown(KeyCode.Space)) return;
+        localInventory.ExchangeAllToInventory(GlobalInventory.Instance);
+        localInventory.ClearInventory();
+    }
+}
