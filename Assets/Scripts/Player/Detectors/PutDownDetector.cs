4e0eb79ea959c6d39db7845bf7d44c42ac09ea35
Coen Steuns <c.steuns@gmail.com>
2019-12-14 13:30:44 +0100

    started detector

--- Assets/Scripts/Player/Detectors/PutDownDetector.cs
+++ Assets/Scripts/Player/Detectors/PutDownDetector.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PutDownDetector : MonoBehaviour
+public class PutDownDetector : Detector
 {
-    
+    protected override void OnTriggerStay(Collider collider)
+    {
 
 
+
+    }
 }
