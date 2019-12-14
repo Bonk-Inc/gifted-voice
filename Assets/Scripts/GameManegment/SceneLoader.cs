fa9cf751b7a9ceca162e98079810cedda7771459
Coen Steuns <c.steuns@gmail.com>
2019-12-14 02:43:54 +0100

    Sceneloading and loadingscreen

---
+++ Assets/Scripts/GameManegment/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private LoadScreenManager loadScreen;
+
+    private void Start()
+    {
+        loadScreen = LoadScreenManager.Instance;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        var operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+        loadScreen.LoadUntil(() => {
+            return operation.progress == 1;
+        }, () => {
+            operation.allowSceneActivation = true;
+        });
+    }
+
+
+}
