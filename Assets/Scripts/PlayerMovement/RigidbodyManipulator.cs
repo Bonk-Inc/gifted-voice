b912bac4bba7f02ff9e0ec44a442bd48fc803c4f
Coen Steuns <c.steuns@gmail.com>
2019-12-14 03:40:00 +0100

    Movement :)

---
+++ Assets/Scripts/PlayerMovement/RigidbodyManipulator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodyManipulator : MonoBehaviour
+{
+    [SerializeField]
+    protected Rigidbody rb;
+
+    private void Reset()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}
