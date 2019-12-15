dc54280e56b1770ae6f7122acef24c6cc5d9dbeb
jvdwijk <joeyvdwijk@gmail.com>
2019-12-15 20:21:01 +0100

    fire & new rooms

---
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField]
+    private LocalInventory inventory;
+
+    [SerializeField]
+    private TMPro.TextMeshProUGUI inventoryText;
+
+
+    private void Start()
+    {
+        inventory.OnInventoryUpdate += UpdateInventoryUI;
+    }
+
+    private void UpdateInventoryUI()
+    {
+        inventoryText.text = inventory.GetAllSlots().Length.ToString();
+    }
+}
