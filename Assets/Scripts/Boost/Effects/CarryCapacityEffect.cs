1be1ba385ab3b73bd7a807dcc559f8a725cc5b1b
Coen Steuns <c.steuns@gmail.com>
2019-12-15 10:12:01 +0100

    Carry capacity

---
+++ Assets/Scripts/Boost/Effects/CarryCapacityEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryCapacityEffect : BoostEffect
+{
+    [SerializeField]
+    private LocalInventory inventory;
+
+    [SerializeField]
+    private int addedCapacity;
+
+    public override BoostType Type => BoostType.carryCapacity;
+
+    public override void Boost()
+    {
+        inventory.MaxInventorySlots += addedCapacity;
+    }
+}
