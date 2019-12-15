69613c675c423c90b641b9b01d7f310cb48d8cf3
Coen Steuns <c.steuns@gmail.com>
2019-12-15 14:31:39 +0100

    Door, calender and score

---
+++ Assets/Scripts/Player/Karels Room/KarelsCalender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarelsCalender : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private TMPro.TextMeshProUGUI calenderText;
+
+    private void Start()
+    {
+        print("last: " + RoundManager.Instance.LastRound + "  Current: " + RoundManager.Instance.CurrentRound);
+        calenderText.text = Mathf.Abs(RoundManager.Instance.LastRound - RoundManager.Instance.CurrentRound).ToString();
+    }
+
+}
---
+++ Assets/Scripts/Player/Karels Room/KarelsDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarelsDoor : MonoBehaviour
+{
+
+    private Collider colly;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            colly = other;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            colly = null;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (colly != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            WorldStateMachine.Instance.NextState();
+        }
+    }
+
+}
